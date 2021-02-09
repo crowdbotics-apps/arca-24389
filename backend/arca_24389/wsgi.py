f3cabce68a29c18529e80fffe1b7aa1714f01d45
Crowdbotics <team@crowdbotics.com>
2021-02-09 02:16:14 +0000

    Update models

--- backend/arca_24389/wsgi.py
+++ backend/arca_24389/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arca_24389.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arca_24389.settings")
 
 application = get_wsgi_application()
